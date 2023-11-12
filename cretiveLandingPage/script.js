4508933d3c292862b670a7ee9e2fcf24fbaa0eae
Aziza <105348844+ai24-m@users.noreply.github.com>
2023-11-12 15:05:57 +0300

    prodacts website

---
+++ cretiveLandingPage/script.js
@@ -0,0 +1,11 @@
+let iphone= document.querySelector('.iphone');
+let container= document.querySelector('.container');
+
+
+function phones(phone){
+    iphone.src = phone;
+}
+
+function bcgColors(color){
+    container.style.background = color;
+}
